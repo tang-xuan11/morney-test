31360685465f9fcd808820490250b8b0398f6c96
tang <923233700@qq.com>
2020-08-23 11:22:26 +0800

    添加 404 页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,8 @@ import Home from "../views/Home.vue";
 import Money from "@/views/Money.vue";
 import Labels from "@/views/Labels.vue";
 import Statistics from "@/views/Statistics.vue";
+import NotFound from '@/views/NotFound.vue'
+import { component } from 'vue/types/umd';
 
 Vue.use(VueRouter);
 
@@ -24,6 +26,10 @@ const routes: Array<RouteConfig> = [
     path: "/statistics",
     component: Statistics,
   },
+  {
+    path: '*',
+    component: NotFound
+  }
 ];
 
 const router = new VueRouter({
