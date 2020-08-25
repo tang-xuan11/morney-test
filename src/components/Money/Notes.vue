7bb263117efa576a9aab8bdc6be86f3ac844da99
tang <923233700@qq.com>
2020-08-25 20:14:03 +0800

    money 模块化

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <label class="notes">
+        <span class="name">备注</span>
+        <input type="text" placeholder="在这里输入备注" />
+      </label>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  font-size: 16px;
+  background: #f5f5f5;
+  display: flex;
+  align-items: center;
+  padding-left: 16px;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    padding: 20px 0;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,190 +1,27 @@
 <template>
   <Layout class-prefix="layout">
-    <div class="numberPad">
-      <div class="output">100</div>
-      <div class="buttons">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button class="ok">OK</button>
-        <button class="zero">0</button>
-        <button>.</button>
-      </div>
-    </div>
-    <div>
-      <ul class="types">
-        <li class="selected">支出</li>
-        <li>收入</li>
-      </ul>
-    </div>
-    <div>
-      <label class="notes">
-        <span class="name">备注</span>
-        <input type="text" placeholder="在这里输入备注" />
-      </label>
-    </div>
-    <div class="tags">
-      <div class="new">
-        <button>新增标签</button>
-      </div>
-      <ul class="current">
-        <li>衣</li>
-        <li>食</li>
-        <li>住</li>
-        <li>行</li>
-      </ul>
-    </div>
+    <NumberPad/>
+    <Types/>
+    <Notes/>
+    <Tags/>
   </Layout>
 </template>
 
 <script lang="ts">
+import Tags from "@/components/Money/Tags.vue";
+import Notes from "@/components/Money/Notes.vue";
+import Types from "@/components/Money/Types.vue";
+import NumberPad from "@/components/Money/NumberPad.vue";
 export default {
+  components: { Tags, Notes, Types, NumberPad },
   name: "Money",
 };
 </script>
 
 <style lang="scss">
-.layout-content {
+.layout-content{
   display: flex;
   flex-direction: column-reverse;
-}
-</style>
-
-<style lang="scss" scoped>
-@import "~@/assets/style/helper.scss";
-.numberPad {
-  .output {
-    @extend %innerShadow;
-    font-size: 36px;
-    font-family: Consolas, monospace;
-    padding: 9px 16px;
-    text-align: right;
-  }
-  .buttons {
-    @extend %clearFix;
-    button {
-      float: left;
-      width: 25%;
-      height: 64px;
-      background: transparent;
-      border: none;
-      &.ok {
-        height: 64 * 2px;
-        float: right;
-      }
-      &.zero {
-        width: 25% * 2;
-      }
-      $bg: #f2f2f2;
-      &:nth-child(1) {
-        background: $bg;
-      }
-      &:nth-child(2),
-      &:nth-child(5) {
-        background: darken($bg, 4px);
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background: darken($bg, 4 * 2px);
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background: darken($bg, 4 * 3px);
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background: darken($bg, 4 * 4px);
-      }
-      &:nth-child(14) {
-        background: darken($bg, 4 * 5px);
-      }
-      &:nth-child(12) {
-        background: darken($bg, 4 * 6px);
-      }
-    }
-  }
-}
-.types {
-  background: #c4c4c4;
-  display: flex;
-  text-align: center;
-  font-size: 24px;
-  li {
-    width: 50%;
-    line-height: 64px;
-    height: 64px;
-    position: relative;
-    &.selected::after {
-      content: "";
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      height: 4px;
-      background: #333;
-    }
-  }
-}
-.notes {
-  font-size: 16px;
-  background: #f5f5f5;
-  display: flex;
-  align-items: center;
-  padding-left: 16px;
-  .name {
-    padding-right: 16px;
-  }
-  input {
-    padding: 20px 0;
-    flex-grow: 1;
-    background: transparent;
-    border: none;
-    padding-right: 16px;
-  }
-}
-.tags {
-  display: flex;
-  flex-direction: column-reverse;
-  flex-grow: 1;
-  font-size: 14px;
-  padding: 16px;
-  > .current {
-    display: flex;
-    flex-wrap: wrap;
-    > li {
-      background: #d9d9d9;
-      $h: 24px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: $h;
-      border-radius: $h/2;
-      padding: 0 16px;
-      margin-right: 12px;
-      margin-top: 4px;
-    }
-  }
-  > .new {
-    padding-top: 16px;
-    > button {
-      background: transparent;
-      border: none;
-      color: #999;
-      border-bottom: 1px solid;
-      padding: 0 4px;
-    }
-  }
 }
 </style>
 
