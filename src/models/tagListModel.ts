10b13a66dfb7726478e915e2cb1989383a079a10
tang <923233700@qq.com>
2020-08-29 18:08:39 +0800

    将 tag 的 string 改为 {id: name , name: name}

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,19 +1,23 @@
 const localStorageKeyName = "tagList";
-
+type Tag = {
+  id: string;
+  name: string;
+};
 type TagListModel = {
-  data: string[];
+  data: Tag[];
   create: (name: string) => "success" | "duplicated";
-  fetch: () => string[];
+  fetch: () => Tag[];
   save: () => void;
 };
 
 const tagListModel: TagListModel = {
   data: [],
   create(name: string) {
-    if (this.data.indexOf(name) >= 0) {
+    const names = this.data.map((item) => item.name);
+    if (names.indexOf(name) >= 0) {
       return "duplicated";
     }
-    this.data.push(name);
+    this.data.push({ id: name, name: name });
     this.save();
     return "success";
   },
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,11 @@
+<template>
+  <Layout>编辑标签</Layout>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default class EditLabel extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+</style>
