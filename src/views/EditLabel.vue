1704cbdb2148b13f563f190ca5757b02fa2aa1d3
tang <923233700@qq.com>
2020-09-01 14:38:12 +0800

    把 tag 改为 currentTag

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,7 @@
       <span class="rightIcon"></span>
     </div>
     <div class="form-wapper">
-      <FormItem :value="tag.name" @update:value="update" fieldName="标签名" placeholder="请输入标签名" />
+      <FormItem :value="currentTag.name" @update:value="update" fieldName="标签名" placeholder="请输入标签名" />
     </div>
     <div class="button-wapper">
       <Button @click="remove">删除标签</Button>
@@ -23,25 +23,25 @@ import { Component } from "vue-property-decorator";
   components: { FormItem, Button },
 })
 export default class EditLabel extends Vue {
-  get tag() {
+  get currentTag() {
     return this.$store.state.currentTag;
   }
   created() {
     const id = this.$route.params.id;
     this.$store.commit("fetchTags");
     this.$store.commit("setCurrentTag", id);
-    if (!this.tag) {
+    if (!this.currentTag) {
       this.$router.replace("/404");
     }
   }
   update(name: string) {
-    if (this.tag) {
-      this.$store.commit("updateTag", { id: this.tag.id, name });
+    if (this.currentTag) {
+      this.$store.commit("updateTag", { id: this.currentTag.id, name });
     }
   }
   remove() {
-    if (this.tag) {
-      this.$store.commit("removeTag", this.tag.id);
+    if (this.currentTag) {
+      this.$store.commit("removeTag", this.currentTag.id);
     }
   }
   goBack() {
